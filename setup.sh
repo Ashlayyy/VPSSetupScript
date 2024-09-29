ae891893cc821ead7377d719b77263dbd642b542
Ashlay Steur <ashlaysteur@intal.nl>
2024-09-29 21:38:31 +0200

    Refactor apt keyring setup in setup.sh

--- setup.sh
+++ setup.sh
@@ -258,10 +258,10 @@ ConfigurePackages() {
     sudo apt update -y
     mkdir -p /etc/apt/keyrings
     rm -rf /etc/apt/keyrings/nodesource.gpg
+    rm -rf /etc/apt/keyrings/archive.heckel.io.gpg
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     NODE_MAJOR=22
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-    sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://archive.heckel.io/apt/pubkey.txt | sudo gpg --dearmor -o /etc/apt/keyrings/archive.heckel.io.gpg
     sudo apt-get install apt-transport-https
     sudo sh -c "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/archive.heckel.io.gpg] https://archive.heckel.io/apt debian main' \
