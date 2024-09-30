5e023a81a7754ac99abf59c9e6a71924beac2aa8
Ashlay Steur <ashlay.prive@gmail.com>
2024-09-30 18:49:38 +0200

    update

--- sed-command.sh
+++ sed-command.sh
@@ -1,6 +1,11 @@
-sed '/\#sslLocation,/ {
-    N
-    s/#sslLocation/ssl_session_timeout 1d;\nssl_session_cache shared:MozSSL:10m;\nssl_session_tickets off;\nssl_protocols TLSv1.3;\nssl_prefer_server_ciphers off;\nssl_stapling on;\nssl_stapling_verify on/;
-}' $1
+sed '/#sslLocation/c\
+> ssl_session_timeout 1d;\
+> ssl_session_cache shared:MozSSL:10m;\
+> ssl_session_tickets off;\
+> ssl_protocols TLSv1.3;\
+> ssl_prefer_server_ciphers off;\
+> ssl_stapling on;\
+> ssl_stapling_verify on/;' $1
+
 echo "Sed command executed successfully"
 echo "File $1 has been modified"
